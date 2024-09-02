7afca4275250a274408a0b6eca662aa47b9d7b6e
{Nitish} <{ns24492449@gmail.com}>
2024-09-02 17:39:26 +0530

    first commit

---
+++ 1st.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+string a,b,c;
+cout <<"Enter the first string: ";
+cin>>a;
+cout<<"Enter the second string: ";
+cin>>b;
+
+cout<<"Concatenated string: "<<a+b<<endl;
+
+    return 0;
+}
